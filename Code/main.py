accba362f11a10fa0670f661847d154f9e6c9567
Lennon <39248842+wintonml@users.noreply.github.com>
2022-05-27 08:15:04 +1200

    Prototype binary counter functionality
    When all fingers are up it will correctly display 1023 (if there are 10 fingers up) or 31 (if there are 5 fingers).
    However, regardless of the hand posittion (thumb left of hand or right) it only is counted as one

--- Code/main.py
+++ Code/main.py
@@ -30,6 +30,9 @@ import mediapipe as mp
 
 from random import randint
 
+FINGER_COORD = [(8, 6), (12, 10), (16, 14), (20, 18)]
+THUMB_COORD = (4, 2)
+
 
 def convert_coords_to_pixels(hand_lms, hand_list, image):
     # Convert each of the co-ordinates for every landmark to pixel
@@ -63,38 +66,59 @@ def determine_thumb_position(hand_list):
     return thumb_left
 
 
-def finger_count(finger_coord, thumb_coord, hand_list, thumb_left,
-                 count_decimal):
+def finger_count(finger_coord, thumb_coord, hand_list, thumb_left, hand_num,
+                 tot_num_of_hands, decimal, binary):
     """
-    Returns the number of fingers/thumb that is up
+    Returns the number of fingers/thumbs that are up.
+    Additionally, it calculates the binary representation if each finger is a
+    binary digit.
+    The counter also starts from the leftmost hand to the right.
+    Leftmost finger on screen is 2**(5*tot_num_hands). Rightmost is 2**0
+
     Parameter:
         finger_coord (list): tuple containing the finger joints
         thumb_coord (list): tuple containing thumb joints
         hand_list (list): tuple containing the joints coords
         thumb_left (bool): position if thumb is left of wrist
-        count_decimal (bool): determines if counting in decimal
+        hand_num (int): number of the hand that is being counted
+        tot_num_of_hands (int): total number of hands on screen
+        decimal (int): current total number of fingers counted
+        binary (int): current total of binary number represented
 
-    return (int): Number of fingers up
+    return (tuple): Number of fingers up, binary count
     """
-    up_count = 0
-    for coordinate in finger_coord:
+    #  Exponent counted from number of hands times fingers minus one as the
+    #  last finger is in the position 2**0.
+    binary_exponent = ((tot_num_of_hands - hand_num) * 5) - 1
+    if thumb_left:
+        # TODO: Change logic to account for thumb y position instead of
+        #  just x position
+        if hand_list[thumb_coord[0]][0] < hand_list[thumb_coord[1]][0]:
+            decimal += 1
+            binary += 2**binary_exponent
+            binary_exponent -= 1
+
+    finger_binary_constant = binary_exponent
+    for finger_index in range(len(finger_coord)):
+        coordinate = finger_coord[finger_index]
+        binary_exponent = finger_binary_constant - finger_index
         if (hand_list[coordinate[0]][1] <
                 hand_list[coordinate[1]][1]):
-            up_count += 1
-    # This uses the x-position of thumb. Can be changed in future to consider
-    # y-position
-    if thumb_left:
-        if hand_list[thumb_coord[0]][0] < hand_list[thumb_coord[1]][0]:
-            up_count += 1
-    else:
+            decimal += 1
+            binary += 2**binary_exponent
+
+    if not thumb_left:
+        binary_exponent -= 1
         if hand_list[thumb_coord[0]][0] > hand_list[thumb_coord[1]][0]:
-            up_count += 1
-    return up_count
+            decimal += 1
+            binary += 2**binary_exponent
+    return decimal, binary
 
 
 def keyboard_input(count_decimal, success):
     """
     This handles the input from the keyboard
+
     Parameter:
         count_decimal (bool): Determines if counting in decimal or binary
         success (bool): Determines if program continues
@@ -112,20 +136,24 @@ def keyboard_input(count_decimal, success):
     return count_decimal, success
 
 
-def display_text(image, up_count, count_decimal):
+def display_text(image, decimal, binary, count_decimal):
     """
     This puts the text on the image for when it is shown.
+
     Parameter:
         image (ndarray): array used to represent the image
-        up_count (int): number of fingers that are up
+        decimal (int): decimal representation of fingers that are up
+        binary (int): binary number calculated
         count_decimal (bool): determines if it is counting in decimal
     return: None
     """
     if count_decimal:
         count_str = "Decimal"
+        display_number = decimal
     else:
         count_str = "Binary"
-    cv2.putText(image, str(up_count), (150, 150),
+        display_number = binary
+    cv2.putText(image, str(display_number), (150, 150),
                 cv2.FONT_HERSHEY_PLAIN, 12, (0, 255, 0), 12)
     cv2.putText(image, f"Counting in {count_str}", (150, 200),
                 cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
@@ -140,10 +168,9 @@ def main():
     success, image = cap.read()
 
     mp_hands = mp.solutions.hands  # Used to detect hands in the input image
-    hands = mp_hands.Hands()  # Used to process the detected hands
+    hands = mp_hands.Hands(max_num_hands=6)  # Used to process the detected
+    # hands
     mp_draw = mp.solutions.drawing_utils  # Used to draw the hands
-    finger_coord = [(8, 6), (12, 10), (16, 14), (20, 18)]
-    thumb_coord = (4, 2)
     hand_dict = {}
     count_decimal = True
 
@@ -155,13 +182,16 @@ def main():
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = hands.process(rgb_image)
         multi_land_marks = results.multi_hand_landmarks
-        up_count = 0
+        decimal = 0
+        binary = 0
 
         # Nested for loop used to draw each of the landmarks for each hand,
         # if multiple are present
         if multi_land_marks:
-            for hand_num in range(len(multi_land_marks)):
+            tot_num_of_hands = len(multi_land_marks)
+            for hand_num in range(tot_num_of_hands):
                 hand_list = []
+
                 if hand_num not in hand_dict:
                     hand_dict[hand_num] = (randint(0, 255), randint(0, 255),
                                            randint(0, 255))
@@ -172,10 +202,11 @@ def main():
                 hand_list = convert_coords_to_pixels(hand_lms, hand_list, image)
                 draw_points(hand_list, image, colour)
                 thumb_left = determine_thumb_position(hand_list)
-                up_count += finger_count(finger_coord, thumb_coord, hand_list,
-                                         thumb_left, count_decimal)
+                decimal, binary = finger_count(
+                    FINGER_COORD, THUMB_COORD, hand_list, thumb_left,
+                    hand_num, tot_num_of_hands, decimal, binary)
         count_decimal, success = keyboard_input(count_decimal, success)
-        display_text(image, up_count, count_decimal)
+        display_text(image, decimal, binary, count_decimal)
 
         cv2.imshow("Counting number of fingers", image)
 
